2df8ab93dd0931cc77697f843bcd8e7993a4a450
Gloriasc419 <62006626+Gloriasc419@users.noreply.github.com>
2020-03-29 15:02:50 -0700

    Delete local_test.py

--- local_test.py
+++
@@ -1,8 +0,0 @@
-people = ["leSlEy", "KEN", "sYDNey", "AnNa", "gRANT"]
-
-peopleNameCap=[names.upper() for names in people]
-print(peopleNameCap)
-
-def printInvite(name):
-    for name in PeopleNameCap:
-        print(f"I would like to invite: {name}")
